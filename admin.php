737097fd567ddd79214f8afae9ad5e3043fb232c
Alange39 <aaange941@gmail.com>
2025-04-03 21:45:57 +0100

    autre commit

--- admin.php
+++ admin.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <html lang="en">
@@ -8,5 +10,8 @@
 </head>
 <body class="bg-[url('assets/images/ananas-large.jpg')]">
     <h1>BIENVENUE</h1>
+
+
+    <?php include_once"tableau.php";?>
 </body>
 </html>
